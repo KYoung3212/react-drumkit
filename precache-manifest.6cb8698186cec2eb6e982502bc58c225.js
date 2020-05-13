53b75e6310177f7926fdacee8122a011687d4f2a
kyoung3212 <kevin.young3212@gmail.com>
2020-05-13 13:20:15 -0700

    Updates

---
+++ precache-manifest.6cb8698186cec2eb6e982502bc58c225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2146472ca961ee85734de1969413729",
+    "url": "/react-drumkit/index.html"
+  },
+  {
+    "revision": "1c9e7b25e32aeac68a10",
+    "url": "/react-drumkit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ecda95301f63aff0a93",
+    "url": "/react-drumkit/static/js/2.b8006472.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-drumkit/static/js/2.b8006472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c9e7b25e32aeac68a10",
+    "url": "/react-drumkit/static/js/main.41d69df9.chunk.js"
+  },
+  {
+    "revision": "c9162cee12bb1bfa72d2",
+    "url": "/react-drumkit/static/js/runtime-main.7395ce39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-drumkit/static/media/logo.5d5d9eef.svg"
+  }
+]);
